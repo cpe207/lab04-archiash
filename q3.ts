99a1cfd62041219571eec8f4021ae619b58d1d33
archiash <46347714+archiash@users.noreply.github.com>
2024-07-01 14:37:09 +0700

    TRUE FALSE TO YES NO

--- q3.ts
+++ q3.ts
@@ -1,7 +1,7 @@
 function primeNumber(a) {
-  if(a % 2 == 0) return a == 2 && a % 2 == 0;
-  for(let i = 3; i * i <= a; i += 2) if(a % i == 0) return false;  
-  return true;
+  if(a % 2 == 0) return (a == 2 && a % 2 == 0) ? "YES" : "NO" ;
+  for(let i = 3; i * i <= a; i += 2) if(a % i == 0) return "NO";  
+  return "YES";
 }
 
 const d1 = 10;
